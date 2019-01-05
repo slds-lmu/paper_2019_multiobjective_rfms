e7704799c9e4a9e94255195a3d9445c108fbe0e1
pfistfl <pfistererf@googlemail.com>
2019-01-05 21:07:16 +0100

    Refactor pre

--- R/pre_bt.R
+++ R/pre_bt.R
@@ -1,7 +1,7 @@
 # run this file to create input data files to save time for
 dumpOMLTask = function(tid = 3891) {
-  library(mlr)
-  mlr:::setMlrOption("show.info", T)
+  require(mlr)
+  mlr:::setMlrOption("show.info", TRUE)
   ot = OpenML::getOMLTask(tid)
   mt = OpenML::convertOMLTaskToMlr(ot)
   task_mlr = mt$mlr.task
@@ -9,47 +9,32 @@ dumpOMLTask = function(tid = 3891) {
   save(task_mlr, file = taskstr)
 }
 
-createRandStratifPartition = function(taskid = 3891) {
+createRandStratifPartition = function(taskid = 3891, nsplits = 5) {
+  # Download Task
   require(OpenML)
   ot = OpenML::getOMLTask(taskid)
   mt = OpenML::convertOMLTaskToMlr(ot)
   task = mt$mlr.task
   df = getTaskData(task)
-  assert(!"dataset_accn" %in% colnames(df))
+
+  # Stratify
   desc = task$task.desc
-  desc$positive
-  desc$negative
-  ind_p = which(df[, desc$target] == desc$positive)
-  ind_n = which(df[, desc$target] == desc$negative)
-  dfp = df[ind_p, ]
-  dfn = df[ind_n, ]
-  pi = sample(1:nrow(dfp))
-  ni = sample(1:nrow(dfn))
-  data_accnp = rep(1:5, length.out = nrow(dfp))
-  data_accnn = rep(1:5, length.out = nrow(dfn))
-  dfp$dataset_accn = data_accnp[pi]
-  dfn$dataset_accn = data_accnn[ni]
+  dfp = df[which(df[, desc$target] == desc$positive), ]
+  dfn = df[which(df[, desc$target] == desc$negative), ]
+
+  posi = sample(seq_len(nrow(dfp)))
+  negi = sample(seq_len(nrow(dfn)))
+
+  data_accnp = rep(seq_len(nsplits), length.out = nrow(dfp))
+  data_accnn = rep(seq_len(nsplits), length.out = nrow(dfn))
+  dfp$dataset_accn = data_accnp[posi]
+  dfn$dataset_accn = data_accnn[negi]
   df = rbind(dfp, dfn)
-  list_dataset_index = lapply(1:5, function(i) which(df[, "dataset_accn"] == i))
-  names(list_dataset_index) = as.character(1:5)
-  df_dataset_accn = as.character(df$dataset_accn)
-  df_dataset_accn = as.factor(df_dataset_accn)
-  # makeResampleInstance always generate the same split which is not wanted
+
+  list_dataset_index = split(seq_len(nrow(df)), df$dataset_accn)
+  df_dataset_accn = as.factor(as.character(df$dataset_accn))
+  
+  # list_dataset_index is a list of indices for each dataset.
+  # df_dataset_accn    is a vector of which datasite an operation is from.
   return(list(task = task, list_dataset_index = list_dataset_index, df_dataset_accn = df_dataset_accn))
-}
-
-
-selectOMLDataSet = function() {
-  task.ids = OpenML::getOMLStudy("OpenML100")$tasks$task.id
-  OpenML::listOMLStudies()
-  require(OpenML)
-  idt = lapply(task.ids, function(id) {
-    ot = OpenML::getOMLTask(id)
-    mt = convertOMLTaskToMlr(ot)
-    sum(mt$mlr.task$task.desc$n.feat) >  0.2 * (mt$mlr.task$task.desc$size)
-   }
-   )
-  idt = which(unlist(idt))
-  idt = task.ids[idt]
-  print(idt)
 }
