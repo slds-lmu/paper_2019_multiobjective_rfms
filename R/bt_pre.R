d939a31dbab940a20cdd3d15f2aba1eb5da87d61
smilesun <smilesun.east@gmail.com>
2019-01-08 21:43:59 +0100

    R create directory

--- R/bt_pre.R
+++ R/bt_pre.R
@@ -1,6 +1,7 @@
 source("utilities_datasite.R")
 # 3891, 9950, 9981, 14966, 34536
 try({OpenML::populateOMLCache(task.ids = c(3891, 9950, 9981, 14966, 34536))})
+if(!dir.exists("../Data/temp")) dir.create("../Data/temp")
 
-list.tname = create_rdata_cluster(pca_var_ratio = 0.7, tids = c(3891), n_datasets = 5, balanced = T)
+list.tname = create_rdata_cluster(pca_var_ratio = 0.7, tids = c(3891, 9950, 9981, 14966, 34536), n_datasets = 5, balanced = T)
 list.tname
