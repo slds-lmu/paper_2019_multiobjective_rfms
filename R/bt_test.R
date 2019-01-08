ce3444a62d5cb410c055d65e684a1a861a3c9518
smilesun <smilesun.east@gmail.com>
2019-01-08 23:31:32 +0100

    .

--- R/bt_test.R
+++ R/bt_test.R
@@ -1,4 +1,5 @@
 # prob_Inputs_data depends on bt_prob.R
+source("utilities_datasite.R")
 prob_inputs_data = list()  # used for addProblem
 prob_inputs_data$geo = prepareDataSite(path = "../Data/data_cohorts_nonGerman.RData", dataset_id = "dataset_accn", targetname = "response")  # this data comes with repository
 #prob_inputs_data$oml3891 = prepareDataSite(path = "../Data/temp/oml_3891_clustered_classbalanced_TRUE.RData",  dataset_id = "dataset_accn", targetname = "label")
