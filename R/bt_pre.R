7db51a77e0ad2d66db40aa0f85cb87d17929f145
smilesun <smilesun.east@gmail.com>
2019-01-09 00:58:50 +0100

    .

--- R/bt_pre.R
+++ R/bt_pre.R
@@ -4,13 +4,13 @@ if (!dir.exists("../output")) dir.create("../output")
 #
   source("utilities_datasite.R")
   # 3891, 9950, 9981, 14966, 34536
-  task.ids = c(3891, 9950, 9981, 14966, 34536)
+  task.ids = c(3891, 9950, 9981)
   try({OpenML::populateOMLCache(task.ids = task.ids)})
   create_rdata_cluster(pca_var_ratio = 0.7, tids = task.ids, n_datasets = 5, balanced = T)
   list.tname = lapply(task.ids, getOMLTaskTargetName)
   list.tname
 #
-  dataset_names_input = c("oml9981", "oml14966", "oml34536")
+  dataset_names_input = c("oml3891")
 # list.data$geo = prepareDataSite(path = "../Data/data_cohorts_nonGerman.RData", dataset_id = "dataset_accn", targetname = "response")  # this data comes with repository
   prob_inputs_data = lapply(1:length(task.ids), function(i) {
     taskid = task.ids[i]
