e5cd2167c250bac6f8d87f9e14ad37c8e6768343
Andrea Bommert <bommert@statistik.tu-dortmund.de>
2019-01-10 17:29:24 +0100

    add error bar plots

--- R/utility_andrea.R
+++ R/utility_andrea.R
@@ -111,8 +111,15 @@ lapply(split(res2, paste(res2$lrn, res2$dataset_name, res2$algorithm)), function
 })
 dev.off()
 
-res2m = res2[, list(mean.cdhv = mean(cdhv)),
+res2m = res2[, list(
+  mean.cdhv = mean(cdhv),
+  median.cdhv = median(cdhv),
+  lower = quantile(cdhv, 0.25, type = 2),
+  upper = quantile(cdhv, 0.75, type = 2)
+),
   by = c("lrn", "dataset_name", "algorithm", "openbox_name", "lockbox_name", "iter", "algo")]
+res2m$lower[res2m$iter %% 5 != 0] = NA
+res2m$upper[res2m$iter %% 5 != 0] = NA
 
 pdf("hypervolume_over_time_aggr_mean.pdf", height = 7, width = 10)
 lapply(split(res2m, paste(res2m$lrn, res2m$dataset_name, res2m$algorithm)), function(dat) {
@@ -125,26 +132,53 @@ lapply(split(res2m, paste(res2m$lrn, res2m$dataset_name, res2m$algorithm)), func
 })
 dev.off()
 
+pdf("hypervolume_over_time_aggr_median.pdf", height = 7, width = 10)
+lapply(split(res2m, paste(res2m$lrn, res2m$dataset_name, res2m$algorithm)), function(dat) {
+  ggplot(data = dat, mapping = aes(x = iter, y = median.cdhv, group = algo, color = algo)) +
+    geom_line() +
+    geom_errorbar(mapping = aes(ymin = lower, ymax = upper), position = position_dodge(width = 1)) +
+    ylab("Median currently dominated hypervolume (based on all datasets)") +
+    facet_grid(openbox_name ~ lockbox_name, scales = "free_y") +
+    ggtitle(paste0("lrn: ", dat$lrn[1], ", dataset_name: ", dat$dataset_name[1],
+      ", algorithm: ", dat$algorithm[1]))
+})
+dev.off()
+
 
 # Aggregate over repls, openbox_name and lockbox_name
 pdf("hypervolume_over_time_aggr_quartiles2.pdf", height = 7, width = 7)
-ggplot(data = res2, mapping = aes(x = iter, y = cdhv, group = algo, color = algo)) +
+ggplot(data = res2, mapping = aes(x = iter, y = cdhv, color = algo)) +
   stat_summary(geom = "ribbon",
     fun.ymin = function(x) quantile(x, 0.25, type = 2),
     fun.ymax = function(x) quantile(x, 0.75, type = 2),
     fun.y = function(x) median(x),
     aes(fill = algo), alpha = 0.3) +
   ylab("Quartiles of currently dominated hypervolume (based on all datasets)") +
-  facet_grid(lrn ~ dataset_name + algorithm, scales = "free_y")
+  facet_grid(lrn ~ algo + dataset_name + algorithm, scales = "free_y")
 dev.off()
 
-res2ma = res2[, list(mean.cdhv = mean(cdhv)),
+res2ma = res2[, list(
+  mean.cdhv = mean(cdhv),
+  median.cdhv = median(cdhv),
+  lower = quantile(cdhv, 0.25, type = 2),
+  upper = quantile(cdhv, 0.75, type = 2)
+  ),
   by = c("lrn", "dataset_name", "algorithm", "iter", "algo")]
+res2ma$lower[res2ma$iter %% 5 != 0] = NA
+res2ma$upper[res2ma$iter %% 5 != 0] = NA
 
 pdf("hypervolume_over_time_aggr_mean2.pdf", height = 7, width = 7)
 ggplot(data = res2ma, mapping = aes(x = iter, y = mean.cdhv, group = algo, color = algo)) +
   geom_line() +
   ylab("Mean currently dominated hypervolume (based on all datasets)") +
+  facet_grid(lrn ~ dataset_name + algorithm, scales = "free_y")
+dev.off()
+
+pdf("hypervolume_over_time_aggr_median2.pdf", height = 7, width = 7)
+ggplot(data = res2ma, mapping = aes(x = iter, y = median.cdhv, group = algo, color = algo)) +
+  geom_line() +
+  geom_errorbar(mapping = aes(ymin = lower, ymax = upper), position = position_dodge(width = 1)) +
+  ylab("Median currently dominated hypervolume (based on all datasets)") +
   facet_grid(lrn ~ dataset_name + algorithm, scales = "free_y")
 dev.off()
 
