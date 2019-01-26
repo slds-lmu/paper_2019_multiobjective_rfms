46712597ebd08caaf28f41cb89478f455b748cb5
smilesun <smilesun.east@gmail.com>
2019-01-26 22:14:58 +0100

    plot

---
+++ R/bt_bootstrap.R
@@ -0,0 +1,5 @@
+source("bt_problem.R")
+ instance = funGenProbOracle(data = prob_inputs_data, job = NULL, openbox_ind = 1L, lockbox_ind = 1L, dataset_name = "geo")
+
+
+sample(
--- R/post_inbag_outbag_plot.R
+++ R/post_inbag_outbag_plot.R
@@ -1,33 +1,10 @@
 library(data.table)
 library(ggplot2)
 library(tidyr)
-dt = readRDS("dt_geo_mmce.rds")
+dt = readRDS("dtmmcenew.rds")
 
-dtoutbag = dt[bag == "outbag"]
-dtinbag = dt[bag == "inbag"]
+ggplot2::ggplot(dt, aes(x = algo, y = openbox, fill = bag)) + geom_boxplot() + facet_grid(cols = vars(lrn))
 
+ggplot2::ggplot(dt, aes(x = algo, y = curator, fill = bag)) + geom_boxplot() + facet_grid(cols = vars(lrn))
 
-tran = function(dtg) {
-
-dtg$openbox = apply(as.data.frame(dtg), 1, FUN = function(x) {
-  as.numeric(x[x["openbox_name"]])})
-
-dtg$lockbox = apply(as.data.frame(dtg), 1, FUN = function(x) {
-  as.numeric(x[x["lockbox_name"]])})
-
-dtg = dtg[, .(openbox_name, lockbox_name, openbox, lockbox, algo, lrn)]
-
-
-}
-
-dtoutbag = tran(dtoutbag)
-dtinbag = tran(dtinbag)
-
-ggplot2::ggplot(dtg, aes(x = algo, y = openbox)) + geom_boxplot() + facet_grid(cols = vars(lrn))
-dev.new()
-showp(dtinbag)
-
-ggplot2::ggplot(dtg, aes(x = algo, y = openbox)) + geom_boxplot() + facet_grid(cols = vars(lrn))
-
-which(dtinbag$openbox > 0.8)
-loadResult(430)
+ggplot2::ggplot(dt, aes(x = algo, y = lockbox, fill = bag)) + geom_boxplot() + facet_grid(cols = vars(lrn))
