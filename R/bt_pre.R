941dbca3df31262b6228468ef7572023505a2ef3
bernd_bischl <bernd_bischl@gmx.net>
2019-01-29 10:11:55 +0100

    .

--- R/bt_pre.R
+++ R/bt_pre.R
@@ -1,11 +1,12 @@
 # this file is for tmux+vim users
 if (!dir.exists("../Data/temp")) dir.create("../Data/temp")
 source("utilities_datasite.R")
-createInput = function(task.ids = c(3891), pca_var_ratio = 0.1, class_balance = T, recluster = T, n_datasets = 5, path_regx = "../Data/temp/oml_%s_pca%s_clustered_classbalanced_TRUE.RData") {
+try({
+  OpenML::populateOMLCache(task.ids = task.ids)  # openml can breakdown easily so put it in try clause
+})
 
-  try({
-    OpenML::populateOMLCache(task.ids = task.ids)  # openml can breakdown easily
-  })
+
+createInput = function(task.ids = c(3891), pca_var_ratio = 0.1, class_balance = T, recluster = T, n_datasets = 5, path_regx = "../Data/temp/oml_%s_pca%s_clustered_classbalanced_TRUE.RData") {
 
   if (recluster) create_rdata_cluster(pca_var_ratio = pca_var_ratio, tids = task.ids, n_datasets = 5, balanced = T, regx = path_regx)
 
