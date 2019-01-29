85bcd51ac09e89a6d73ae5f2591e9fa4664b8034
bernd_bischl <bernd_bischl@gmx.net>
2019-01-29 19:08:30 +0100

    .

--- R/post_bt4andrea_agg.R
+++ R/post_bt4andrea_agg.R
@@ -1,6 +1,6 @@
 library(batchtools)
 reg = LoadRegistry("registry1")
 res = reduceResultsList(ids = findDone(), fun = function(job, res) {
-    list(repl = job$repl, prob.pars = job$prob.pars, algo.pars = job$algo.pars, res = res)
+    list(repl = job$repl, prob.pars = job$prob.pars, algo.pars = job$algo.pars, gperf_env = res$res$gperf_env, instance = res$res$instance)
 })
 save(res, file = "DortmundClusterRes.RData")
