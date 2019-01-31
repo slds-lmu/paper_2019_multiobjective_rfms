8ae3961ba548598c7fb3039c8d5c169506bf6636
smilesun <smilesun.east@gmail.com>
2019-01-31 01:15:52 +0100

    add bt4andrea_debugR

---
+++ R/bt4andrea_debug.R
@@ -0,0 +1,11 @@
+source("bt_pre.R")
+DEBUG_FLAG = T # if true: use low budget (only 7 iterations of mbo) set to F
+source("bt_conf.R")
+source("bt_main.R")
+btInit(path = "registry_debg", local = F)   # set local=T to allow on local PC running
+######################################
+mgconf = getGconf()
+reg_input = batchtools::getDefaultRegistry()
+reg_input$default.resources
+init(prob_names, prob_inputs_conf, prob_funs, algo_names, algo_funs, reg_input, algo_designs, repls = mgconf$REPLS)
+#submitJobs(1:600)
